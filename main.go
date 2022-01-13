970523ff678e309c53bbbba17b5527ed35f0fd4b
hellojukay <hellojukay@163.com>
2022-01-13 18:16:03 +0800

    修复终端未自动回复的 bug

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/c-bata/go-prompt"
 	"github.com/chyroc/aliyundrive-cli/internal"
+	"golang.org/x/sys/unix"
 )
 
 func main() {
+	oldTermiosPtr, _ := unix.IoctlGetTermios(int(os.Stdin.Fd()), unix.TCGETS)
+	defer func() {
+        unix.IoctlSetTermios(int(os.Stdin.Fd()),unix.TCSETS,oldTermiosPtr)
+	}()
 	cli := internal.NewCli()
 	fmt.Println("阿里云盘命令行客户端")
 
