2a25c4f109af66265f4df1b1fa47c391b8c9811e
chyroc <chyroc@qq.com>
2022-01-18 13:50:34 +0800

    add ci (#6)

---
+++ internal/ioctl.go
@@ -0,0 +1,15 @@
+//go:build !linux
+// +build !linux
+
+package internal
+
+import (
+	"golang.org/x/sys/unix"
+)
+
+func IoctlGetTermios() *unix.Termios {
+	return nil
+}
+
+func IoctlSetTermios(termios *unix.Termios) {
+}
---
+++ internal/ioctl_linux.go
@@ -0,0 +1,19 @@
+//go:build linux
+// +build linux
+
+package internal
+
+import (
+	"os"
+
+	"golang.org/x/sys/unix"
+)
+
+func IoctlGetTermios() *unix.Termios {
+	termios, _ := unix.IoctlGetTermios(int(os.Stdin.Fd()), unix.TCGETS)
+	return termios
+}
+
+func IoctlSetTermios(termios *unix.Termios) {
+	unix.IoctlSetTermios(int(os.Stdin.Fd()), unix.TCSETS, termios)
+}
