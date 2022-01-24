84665668023915a94803013b511ed9228a8da5b9
hellojukay <hellojukay@163.com>
2022-01-24 14:17:43 +0800

    支持展示程序版本信息 (#7)

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+
+	"runtime/debug"
 
 	"github.com/c-bata/go-prompt"
 	"github.com/chyroc/aliyundrive-cli/internal"
 )
+
+var version bool
+
+func init() {
+	flag.BoolVar(&version, "version", false, "Print program version")
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+	if version {
+		info, ok := debug.ReadBuildInfo()
+		if ok {
+			println(info.Main.Version)
+		}
+		os.Exit(0)
+	}
+}
 
 func main() {
 	oldTermiosPtr := internal.IoctlGetTermios()
