0f77ea5301db062eaaf3c7bdae5e6f9b35c61510
hellojukay <hellojukay@163.com>
2022-02-16 09:40:08 +0800

    支持上传文件夹 (#18)
    * 支持上传文件夹
    
    * 支持他通配符 fixed #17

--- internal/command_upload.go
+++ internal/command_upload.go
@@ -3,6 +3,8 @@ package internal
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -19,8 +21,24 @@ func (r *CommandUpload) Run() error {
 	if err := r.cli.setupDrive(); err != nil {
 		return err
 	}
+	defer r.cli.refreshFiles()
 
 	file := r.file
+	files, err := filepath.Glob(file)
+	if err != nil {
+
+		return err
+	}
+	for _, file := range files {
+		err = r.upload(file, r.cli.driveID, r.cli.currentFileID)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *CommandUpload) upload(file string, driveID string, fileID string) error {
 	if strings.HasPrefix(file, "~") {
 		home, _ := os.UserHomeDir()
 		file = home + file[1:]
@@ -29,21 +47,44 @@ func (r *CommandUpload) Run() error {
 	if err != nil {
 		return err
 	}
-	if _, err := os.Stat(file); err != nil {
-		return err
-	}
-
-	_, err = r.cli.ali.File.UploadFile(context.Background(), &aliyundrive.UploadFileReq{
-		DriveID:  r.cli.driveID,
-		ParentID: r.cli.currentFileID,
-		FilePath: file,
-	})
+	fileInfo, err := os.Stat(file)
 	if err != nil {
 		return err
 	}
-	fmt.Println("上传成功.")
-
-	go r.cli.refreshFiles()
-
+	if !fileInfo.IsDir() {
+		_, err = r.cli.ali.File.UploadFile(context.Background(), &aliyundrive.UploadFileReq{
+			DriveID:  driveID,
+			ParentID: fileID,
+			FilePath: file,
+		})
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s 上传成功.\n", file)
+	} else {
+		// mkdir
+		files, err := ioutil.ReadDir(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		response, err := r.cli.ali.File.CreateFolder(context.Background(), &aliyundrive.CreateFolderReq{
+			DriveID:      r.cli.driveID,
+			ParentFileID: r.cli.currentFileID,
+			Name:         fileInfo.Name(),
+		})
+		if err != nil {
+			return err
+		}
+		if err := r.cli.checkoutDir(response.FileID, fileInfo.Name()); err != nil {
+			return err
+		}
+		for _, subFile := range files {
+			err := r.upload(filepath.Join(file, subFile.Name()), response.DriveID, response.FileID)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("%s 上传成功.\n", filepath.Join(file, subFile.Name()))
+		}
+	}
 	return nil
 }
